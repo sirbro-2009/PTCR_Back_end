1225b909a94481e9cfa09b0058ae4ec92ddf06d6
sirbro-2009 <zernikhmouhamed8@gmail.com>
2026-09-12 12:09:18 +0100

    Add upload own bg to /set_bg route

--- routes/mosque_router/edit_mosque_data.ts
+++ routes/mosque_router/edit_mosque_data.ts
@@ -2,51 +2,60 @@ import express from "express";
 const router = express.Router();
 import type { Request, Response } from "express";
 import Mosque from "../../models/mosque.js";
+import upload from "../../middleware/multer.js";
+
 interface IcamaObject {
-      Fajr: string,
-      Dhuhr: string,
-      asr: string,
-      Maghrib: string,
-      Isha: string,
-    }
-router.post("/edit_icama_durations", async (req: Request, res: Response) =>{
-try{
+  Fajr: string;
+  Dhuhr: string;
+  asr: string;
+  Maghrib: string;
+  Isha: string;
+}
+router.post("/edit_icama_durations", async (req: Request, res: Response) => {
+  try {
     const token = req.headers.authorization?.split(" ")[1];
-    const icama_object:IcamaObject  = req.body
+    const icama_object: IcamaObject = req.body;
     let mosuqe;
     if (token) {
       mosuqe = await Mosque.findOne({ "Token.token": token });
     }
-    if (!mosuqe || !icama_object) return res.status(404).send({ error: "unvalid data" });
-    if(icama_object && mosuqe.MosqueProps){
-    mosuqe.MosqueProps.MosqueIcama = icama_object
-    await mosuqe.save()
-    res.status(200).json(mosuqe.MosqueProps)
+    if (!mosuqe || !icama_object)
+      return res.status(404).send({ error: "unvalid data" });
+    if (icama_object && mosuqe.MosqueProps) {
+      mosuqe.MosqueProps.MosqueIcama = icama_object;
+      await mosuqe.save();
+      res.status(200).json(mosuqe.MosqueProps);
     }
-
-}
-catch(err){
-    res.status(500).json({err})
-}
-})
-router.post("/set_bg", async (req: Request, res: Response) =>{
-try{
-    const token = req.headers.authorization?.split(" ")[1];
-    const {type,link} = req.body
-    let mosuqe;
-    if (token) {
-      mosuqe = await Mosque.findOne({ "Token.token": token });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+router.post(
+  "/set_bg",
+  upload.single("bg_img"),
+  async (req: Request, res: Response) => {
+    try {
+      const token = req.headers.authorization?.split(" ")[1];
+      const { type, link } = req.body;
+      let mosque;
+      if (token) {
+        mosque = await Mosque.findOne({ "Token.token": token });
+      }
+      if (!mosque || !type || !link)
+        return res.status(404).send({ error: "unvalid data" });
+      if (type === "0" && mosque.MosqueProps && !req.file) {
+        mosque.MosqueProps.MosqueImg = link;
+        await mosque.save();
+        res.status(200).json(mosque.MosqueProps);
+      }
+      if (type === "1" && req.file && mosque.MosqueProps) {
+        const bg_url = req.file.path;
+        mosque.MosqueProps.MosqueImg = bg_url;
+        await mosque.save();
+      }
+    } catch (err) {
+      res.status(500).json({ err });
     }
-    if (!mosuqe || !type ||!link) return res.status(404).send({ error: "unvalid data" });
-    if(type==='0' && mosuqe.MosqueProps){
-    mosuqe.MosqueProps.MosqueImg = link 
-    await mosuqe.save()
-    res.status(200).json(mosuqe.MosqueProps)
-    }
-
-}
-catch(err){
-    res.status(500).json({err})
-}
-})
-export default router
+  },
+);
+export default router;
